68a537615aea7f919f7512ce337b3831d1c2af59
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2023-05-18 15:24:34 +0200

    init commit

---
+++ scripts/init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install terraform
+apt-get update && apt-get install -y gnupg software-properties-common gettext wget unzip curl
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+  gpg --dearmor | \
+  tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+gpg --no-default-keyring \
+  --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+  --fingerprint
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    tee /etc/apt/sources.list.d/hashicorp.list
+
+apt update && apt-get install terraform
+
+# Install aws-cli
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/opt/awscliv2.zip"
+unzip /opt/awscliv2.zip -d /opt/
+bash /opt/aws/install
+
